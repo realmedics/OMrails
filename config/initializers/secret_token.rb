14fd2fb536e4846f10aa3b9a4cae34fe9fa46924
Trent Starnes <info@realmedics.com>
2013-09-13 14:10:01 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OMRails::Application.config.secret_token = '4bb363cbaa2924000989d7072f20a635223c0df4e4c9b7e592bc3c4ddcb989b9bf0f164501c046482dd77b24e77f32ec93d55d6b98fc2851c5922556928acd28'
