ceed661110e3e34330e4ebe19a9be31798be5183
realmedics <info@realmedics.com>
2013-09-18 09:05:00 -0500

    Connect Amazon S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,15 @@ OMRails::Application.configure do
 
   #In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+# config/environments/production.rb
 
+#/ Configuration for S3/Paperclip/Heroku #/
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
